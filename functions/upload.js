8240f645ea3ec8a7aac1cb128cef8090063697c9
JssDream <jshao817@gmail.com>
2024-09-13 15:39:41 +0800

    Update upload.js
    注释header

--- functions/upload.js
+++ functions/upload.js
@@ -11,7 +11,7 @@ export async function onRequestPost(context) {  // Contents of context object
      const url = new URL(request.url);
      const response = fetch('https://telegra.ph/' + url.pathname + url.search, {
          method: request.method,
-         headers: request.headers,
+         // headers: request.headers,
          body: request.body,
      });
     return response;
